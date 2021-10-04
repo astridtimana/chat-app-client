00bf403319495aefb5ba35b90930c2e6051ff6fd
thainole <thainole@gmail.com>
2021-10-04 13:18:24 -0500

    changing project from js to tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Container, Button } from "react-bootstrap";
+
+function App() {
+  return (
+    <Container fluid>
+      <p>Holis</p>
+      <Button variant="danger">Hola</Button>
+    </Container>
+  );
+}
+
+export default App;
